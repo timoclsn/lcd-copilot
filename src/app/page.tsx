5eee6cbf1e6b40bce7d02174c1d7f8acc11af7c6
Timo Clasen <goebeltimo@gmail.com>
2023-09-30 22:51:59 +0200

    Add Home Link

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import { Answer } from "@/components/Answer";
 import { ResourceInput } from "@/components/ResourceInput";
 import { SearchParams } from "@/lib/types";
+import Link from "next/link";
 import { Suspense } from "react";
 import { z } from "zod";
 
@@ -20,7 +21,9 @@ export default function Home({ searchParams }: Props) {
   return (
     <main className="w-full p-8">
       <section className="max-w-screen-sm mx-auto">
-        <h1 className="text-4xl font-black text-center mb-8">LCD Copilot</h1>
+        <Link href="/" className="hover:underline">
+          <h1 className="text-4xl font-black text-center mb-8">LCD Copilot</h1>
+        </Link>
         <ResourceInput />
         {url && (
           <Suspense fallback={<div>Processing…</div>}>
