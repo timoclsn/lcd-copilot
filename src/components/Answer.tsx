e14ba7d0e494e90a7aeae3a5a08bc55a927c5503
Timo Clasen <goebeltimo@gmail.com>
2023-10-03 22:01:48 +0200

    Update Prompt

--- src/components/Answer.tsx
+++ src/components/Answer.tsx
@@ -1,4 +1,3 @@
-import { prismaSchema } from "@/lib/prompt";
 import OpenAI from "openai";
 import ReactMarkdown from "react-markdown";
 
@@ -12,44 +11,102 @@ interface Props {
 
 export const Answer = async ({ url }: Props) => {
   const urlResponse = await fetch(url);
-  const urlText = await urlResponse.text();
-  const urlTextWithoutScriptContent = urlText.replace(
-    /<script\b[^<]*(?:(?!<\/script>)<[^<]*)*<\/script>/gi,
-    "<script></script>"
-  );
-  const urlTextWithoutTags = urlTextWithoutScriptContent.replace(
-    /<[^>]*>/g,
-    "."
-  );
+  const websiteSource = await urlResponse.text();
+
+  const titleRegex = /<title>(.*?)<\/title>/i;
+  const titleMatch = websiteSource.match(titleRegex);
+  const title = titleMatch ? titleMatch[1] : "";
+
+  const descriptionRegex = /<meta\s+name="description"\s+content="([^"]*)"/i;
+  const descriptionMatch = websiteSource.match(descriptionRegex);
+  const description = descriptionMatch ? descriptionMatch[1] : "";
+
+  const websiteTextClean =
+    websiteSource
+      // Remove script tag content
+      .replace(
+        /<script\b[^<]*(?:(?!<\/script>)<[^<]*)*<\/script>/gi,
+        "<script></script>"
+      )
+      // Remove style tag content
+      .replace(
+        /<style\b[^<]*(?:(?!<\/style>)<[^<]*)*<\/style>/gi,
+        "<style></style>"
+      )
+      // Remove all xml tags
+      .replace(/<[^>]*>/g, "|")
+      // Replace all line breaks
+      .replace(/\r?\n|\r/g, "")
+      // Replace multiple spaces
+      .replace(/ {2,}/g, "")
+      // Replace multiple dots with one
+      .replace(/\.{2,}/g, ". ")
+      .replace(/\|{2,}/g, "|")
+      // Only use the first 3500 characters
+      .substring(0, 10000) + "...";
+
+  const prompt = `
+    After the instructions I am going to give you a markdown table with categroies and the text content of a website as context.
+
+    Instructions:
+    - Your job is to figure out how the website content can be categorized.
+    - Only use the provided categories from the table – never invent new ones!
+    - Answer only with the one category you are most confident with – never list multiple categories!
+    - Fill out the fields for that category like this example: "Field Title: Field Value."
+    - Convert the field names from camel case to title case
+    - Format dates like this example: "Date: September 27, 2023 (2023-09-27)"
+    - If you can't figure out the content for a field just add a -
+    - Don't copy the text from the website – say it in you own words but leave titles and name how they are.
+    - Answer in markdown format with the name of the category as the headline (level 2) and the fields as a unordered list.
+    - Make the field names bold
+
+    \n----------------\n
+
+    CATEGORY TABLE: 
+    | Category | Fields |
+    | --- | --- |
+    | **Thoughtleader** | name, jobDescription |
+    | **Article** | title, authors, date, duration, topics |
+    | **Book** | title, authors, publisher, isbn, topics |
+    | **Podcast** | title, hosts, topics, podcastEpisodes |
+    | **Podcast Episode** | title, podcast, guests, date, duration, topics |
+    | **Directory** | name, description, topics |
+    | **Video** | title, creators, date, duration, topics |
+    | **Tool** | name, description, topics |
+    | **Community** | name, description, topics |
+    | **Course** | name, description, topics |
+    | **Example** | name, description, topics |
+    | **Agency** | name, description, topics |
+    | **Slide** | title, authors, date, topics |
+    | **Magazine** | name, description, topics |
+    | **Newsletter** | name, authors, description, frequency, topics |
+    | **Paper** | title, authors, date, journal, topics |
+    | **Social Media Profile** | name, handle, description, topics |
+    | **Report** | title, authors, date, description, topics |
+
+    WEBSITE TITLE: ${title}
+
+    WEBSITE DESCRIPTION: ${description}
+
+    WEBSITE CONTENT: \`\`\`
+    ${websiteTextClean}
+    \`\`\`
+  `;
+
+  console.log(prompt);
 
   const aiResponse = await openai.chat.completions.create({
     model: "gpt-3.5-turbo",
     stream: false,
     messages: [
       {
+        role: "system",
+        content:
+          "You are my AI web scraper. Your job is to make sense of the text content of a website and put it into a category.",
+      },
+      {
         role: "user",
-        content: `After the instructions I am going to give you a markdown table and the text content of a website. 
-        Your job is to figure out how the website content can be categorized. 
-        Only use the provided categories – never invent new ones! 
-        Answer only with the one category you are most confident with – never list multiple categories! 
-        Fill out the fields for that category like this example: "Field Title: Field Value."
-        Convert the field names from camel case to title case.
-        Format dates like this example: "Date: September 27, 2023 (2023-09-27)".
-        If you can't figure out the a field just add a '-'. 
-        Answer in markdown format with the name of the category as the headline (level 2) and the fields as a unordered list. 
-
-        \n----------------\n
-  
-        TABLE: ${prismaSchema}
-
-        \n----------------\n
-
-        WEBSITE CONTENT:
-
-        \`\`\`
-        ${urlTextWithoutTags.substring(0, 3500)}
-        \`\`\`
-        `,
+        content: prompt,
       },
     ],
   });
